5747899c3bb0fee9d1ca2123f66cd52ad00b7f87
Burak Karaman <karaman.buraak@gmail.com>
2024-04-17 11:01:58 +0200

    Add new pages & routing

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,20 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import authReducer from "../features/authSlice";
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-}
+};
 
-const persistedReducer = persistReducer(persistConfig, authReducer)
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
-export let persistor = persistStore(store)
+export let persistor = persistStore(store);
+
 export default store;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,12 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Home from "../pages/Home";
+import Brands from './../pages/Brands';
+import Firms from "../pages/Firms";
+import Products from "../pages/Products";
+import Sales from "../pages/Sales";
+import Purchases from "../pages/Purchases";
 
 const AppRouter = () => {
   return (
@@ -12,7 +18,21 @@ const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+
+            {/* <Route path="" element={<Home/>}/> */}
+            <Route index element={<Home/>}/>
+
+            {/* Absolute Path */}
+            <Route path="/stock/brands" element={<Brands/>} />
+
+            {/* Relative Path */}
+            <Route path="firms" element={<Firms/>}/>
+            <Route path="products" element={<Products/>}/>
+            <Route path="sales" element={<Sales/>}/>
+            <Route path="purchases" element={<Purchases/>}/>
+
+          </Route>
         </Route>
       </Routes>
     </Router>
