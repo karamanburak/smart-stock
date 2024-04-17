96964ab887995a4b850e34856abe3c83a0c02040
Burak Karaman <karaman.buraak@gmail.com>
2024-04-17 12:48:43 +0200

    Update sidebar & icons

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,24 +1,89 @@
-import MailIcon from "@mui/icons-material/Mail";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
+import { Box } from "@mui/material";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Toolbar from "@mui/material/Toolbar";
-import React from 'react';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const icon = (name) => `/assets/navbar/${name}.svg`
+
+const links = [
+    {
+        title: "Dashboard",
+        url: "/stock",
+        // icon:"/assets/navbar/ic_analytics.svg"
+        icon: icon("ic_analytics"),
+    },
+    {
+        title: "Purchases",
+        url: "/stock/purchases",
+        icon: icon("purchase"),
+    },
+    {
+        title: "Sales",
+        icon: icon("sales"),
+        url: "/stock/sales/",
+    },
+    {
+        title: "Firms",
+        icon: icon("firms"),
+        url: "/stock/firms/",
+    },
+    {
+        title: "Brands",
+        icon: icon("brand"),
+        url: "/stock/brands/",
+    },
+    {
+        title: "Products",
+        icon: icon("ic_cart"),
+        url: "/stock/products/",
+    },
+];
+
+
+const iconStyle = {
+    color: "secondary.main",
+    borderRadius: "1rem",
+    "&:hover": {
+        backgroundColor: "secondary.main",
+        color: "white",
+    },
+};
+const selectedStyle = {
+    backgroundColor: "secondary.second",
+    borderRadius: "1rem",
+    "&:hover": {
+        backgroundColor: "secondary.main",
+        color: "secondary.second",
+    },
+    color: "white",
+};
+
+
 const MenuListItems = () => {
+    const navigate = useNavigate()
+    const {pathname} = useLocation()
+    // console.log(pathname);
     return (
         <div>
             <Toolbar />
             <List>
-                {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
+                {links.map((item, index) => (
+                    <ListItem key={item.title} disablePadding>
+                        <ListItemButton 
+                        onClick={()=>navigate(item.url)} 
+                        sx={pathname == item.url ? selectedStyle : iconStyle}>
+                                <Box sx={{
+                                    width:24,
+                                    height:24,
+                                    mask: `url(${item.icon}) no-repeat center / contain`,
+                                    mr:2,
+                                    bgcolor:"currentColor",
+                                }}/>
+                            <ListItemText primary={item.title} />
                         </ListItemButton>
                     </ListItem>
                 ))}
