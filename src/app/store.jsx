bf5172658d8551a581d4440b7a2d28af4f8b50f5
Burak Karaman <karaman.buraak@gmail.com>
2024-04-17 09:58:54 +0200

    Add redux-persist structure

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,12 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-// import authMidReducer from "../features/authSliceMiddleware"; //!middleware ile olan kullanım
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
-    // auth:authMidReducer,
+    auth: persistedReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
+export let persistor = persistStore(store)
 export default store;
