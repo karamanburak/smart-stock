ee8501e76a70281263024e55073b4dfdd79fdb79
Burak Karaman <karaman.buraak@gmail.com>
2024-04-19 11:32:24 +0200

    Update useStocCall custom hook

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -7,24 +7,27 @@ const useStockCall = () => {
     const dispatch = useDispatch()
     const { token } = useSelector(state => state.auth)
 
+
+    //* DRY
+    //! yukarıdaki gibi her seferinde yazmak yerine daha modüler bir yapı kurarak tek bir fonksiyonla bir den fazla get işlemini gerçekleştirebiliyoruz
     const getStockData = async (url) => {
-        dispatch(fetchStart())
+        dispatch(fetchStart());
         try {
             const { data } = await axios(`${BASE_URL}${url}`, {
                 headers: {
-                    Authorization: `Token ${token}`
-                    // Authorization: `Bearer ${accessToken}` //* JWT (Jason Web Token) icin *\\
-                }
-            })
+                    Authorization: `Token ${token}`,
+                    // Authorization: `Bearer ${accesstoken}` //* jwt için
+                },
+            });
             console.log(data);
-            // dispatch(brandsSuccess(data.data))
-            dispatch(getSuccess({ data: data.data, url })) //* action creatorlar her zaman tek bir parametre kabul ederler *\\
-
+            // dispatch(brandsSuccess(data.data));
+            // dispatch(getSuccess({data:data.data,url:url}));//* action creatorlar her zaman tek bir parametre kabul ederler
+            dispatch(getSuccess({ data: data.data, url }));//* action creatorlar her zaman tek bir parametre kabul ederler
         } catch (error) {
             console.log(error);
-            dispatch(fetchFail())
+            dispatch(fetchFail());
         }
-    }
+    };
 
     return {getStockData }
 };
