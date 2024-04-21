e5970002347a99c226702764fcd795d5c5186bed
Burak Karaman <karaman.buraak@gmail.com>
2024-04-21 08:58:19 +0200

    Update Switch component with changed IconButton styling and functionality

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -10,13 +10,8 @@ const Switch = () => {
     const { mode } = useSelector((state) => state.darkMode)
     const dispatch = useDispatch()
 
-
-
     return <Box sx={{marginBottom:".5rem"}}> 
         {mode} 
-        {/* <IconButton sx={{backgroundColor: mode ? "white " : "black"}} onClick={()=>dispatch(toggleDarkMode())} color="inherit">
-            {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-        </IconButton> */}
         <IconButton sx={{ backgroundColor: mode ? "white" : "primary.main", color: mode ? "#2b3a4c" : "white"}} onClick={()=>dispatch(toggleDarkMode(!mode))} color="inherit">
             {mode === '' ? <Brightness7Icon /> : <Brightness4Icon />}
         </IconButton>
