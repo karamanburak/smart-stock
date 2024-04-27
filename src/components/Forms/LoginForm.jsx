c137be634afc0ac4bafc8e19e90b6d950c09f4de
Burak Karaman <karaman.buraak@gmail.com>
2024-04-27 17:59:22 +0200

    Remove use of useSelector and related loading state in LoginForm; instead, handle button state directly in form

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -2,7 +2,6 @@ import { Button, CircularProgress } from "@mui/material";
 import Box from "@mui/material/Box"
 import TextField from "@mui/material/TextField"
 import { Form } from "formik"
-import { useSelector } from "react-redux"
 import { object, string} from "yup"; //! bu şekilde de direk olarak metodları alıp yine validasyon şemamızı oluşturabiliriz. 
 
 export const loginScheme = object({
@@ -14,7 +13,6 @@ export const loginScheme = object({
 })
 
 const LoginForm = ({ values, handleChange, errors, touched, handleBlur }) => {
-  const { loading } = useSelector(state => state.auth);// storeda yaptığımız fetchStart işlemini kullanmış olduk.
   return (
     <Form>
       <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
@@ -47,15 +45,10 @@ const LoginForm = ({ values, handleChange, errors, touched, handleBlur }) => {
           helperText={touched.password && errors.password}
           error={touched.password && Boolean(errors.password)}
         />
-        {!loading ? (
           <Button variant="contained" type="submit" sx={{ backgroundColor: "secondary.main" }}>
             Sign In
           </Button>
-        ) : (
-          <Button variant="contained" disabled={loading}>
-            <CircularProgress />
-          </Button>
-        )}
+  
       </Box>
     </Form>
   );
