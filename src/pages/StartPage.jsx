4128a4756039639b2844dd953e29fc02723ec51b
Burak Karaman <karaman.buraak@gmail.com>
2024-04-27 17:42:45 +0200

    Update welcome message typography from h4 to h2 in StartPage.jsx

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -16,7 +16,7 @@ const StartPage = () => {
                 <Grid container spacing={2} mt={9} sx={{ height: "80vh", flex }}>
                     <Grid item xs={12} md={4} sx={{ marginRight:"2rem" }}>
                         <Box sx={{ display: "flex", flexDirection: "column", gap: ".5rem", textAlign: "justify", justifyContent: "center", alignItems: "center" }}>
-                            <Typography variant="h4" sx={{ textAlign: "center" }}>Wellcome to Smart Stock</Typography>
+                            <Typography variant="h2" sx={{ textAlign: "center" }}>Wellcome to Smart Stock</Typography>
                             <Typography variant="span" style={{ maxWidth: "500px", fontSize: "1.2rem" }}>
                                 Store your warehouse stock and get a fully automated stockholder register with full security. Smart Stock gives you a platform where you can interact with and manage your stock.
                                 You can also check the current status and update it yourself if necessary. This can be done through a completely simple on-screen interface.
